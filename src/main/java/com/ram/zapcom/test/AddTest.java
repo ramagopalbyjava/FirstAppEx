b106c036c5e723ea8a600d9a61cd10c3dfa736db
Lead <Lead@gmail.com>
2023-02-09 21:22:32 +0530

    Initial commit Add First Application

---
+++ src/main/java/com/ram/zapcom/test/AddTest.java
@@ -0,0 +1,7 @@
+package com.ram.zapcom.test;
+
+public class AddTest {
+    public int addMethod(int a,int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/com/ram/test/Test.java
@@ -0,0 +1,14 @@
+package com.ram.test;
+
+import com.ram.zapcom.test.AddTest;
+
+public class Test {
+    void testAddMethod(){
+        AddTest at=new AddTest();
+        int expectedResult=at.addMethod(2,4);
+        int actualResult=6;
+
+    }
+
+
+}
